50506f24f12738ff60c1bcef90a5f96b1e536f13
seelabs <scott.determan@yahoo.com>
2023-02-07 18:03:29 -0500

    [fold] Remove getFieldObject

--- src/ripple/protocol/STObject.h
+++ src/ripple/protocol/STObject.h
@@ -235,8 +235,6 @@ public:
     getFieldV256(SField const& field) const;
     const STArray&
     getFieldArray(SField const& field) const;
-    const STObject&
-    getFieldObject(SField const& field) const;
 
     /** Get the value of a field.
         @param A TypedField built from an SField value representing the desired
